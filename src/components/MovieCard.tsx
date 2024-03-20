b23a45aa7e651e865ffdd0f337db00b07b863bf6
yash-tile <yash.tile@indexnine.com>
2024-03-20 15:34:04 +0530

    movie title and changed watch later btn position in movie card overlay

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -17,11 +17,14 @@ function MovieCard({ movie }: MovieCardProps) {
         alt={movie.title}
         className="w-full h-auto block rounded-md"
       />
-      <div className="absolute top-0 left-0 w-full h-full opacity-0 hover:bg-black/80 hover:opacity-50 hover:rounded-md text-white">
+      <div className="absolute top-0 left-0 w-full h-full opacity-0 hover:bg-black/80 hover:opacity-70 hover:rounded-md text-white">
+        <p className="text-sm md:text-lg font-bold flex justify-center items-center h-full text-center p-4">
+          {movie.title}
+        </p>
         {isWatchLater ? (
-          <MdWatchLater className="absolute top-4 left-4 text-3xl md:text-5xl hover:text-gray-400" />
+          <MdWatchLater className="absolute top-4 right-4 text-3xl md:text-5xl hover:text-gray-400" />
         ) : (
-          <MdOutlineWatchLater className="absolute top-4 left-4 text-3xl md:text-5xl hover:text-gray-400" />
+          <MdOutlineWatchLater className="absolute top-4 right-4 text-3xl md:text-5xl hover:text-gray-400" />
         )}
       </div>
     </div>
